b7cb76ff524340aea08f3820ab395e9b0cc53d4c
Billy Lin <nixon.software@gmail.com>
2020-03-08 21:15:02 -0700

    Added StationOpenPeriods and StationPlanningStatus to SQL script

--- schema.sql
+++ schema.sql
@@ -1,13 +1,6 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `nixoumcj_energy_manager`
@@ -20,14 +13,14 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `Company` (
-  `name` char(16) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `description` char(64) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `website` char(64) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` char(16) NOT NULL,
+  `description` char(64) NOT NULL,
+  `website` char(64) NOT NULL,
   `isStation` tinyint(1) NOT NULL DEFAULT '1',
   `isSupplier` tinyint(1) NOT NULL DEFAULT '0',
   `isNozzle` tinyint(1) NOT NULL DEFAULT '0',
-  `logo` char(128) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  PRIMARY KEY (`name`);
+  `logo` char(128) DEFAULT NULL,
+  PRIMARY KEY (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -37,10 +30,22 @@ CREATE TABLE `Company` (
 --
 
 CREATE TABLE `Nozzle` (
-  `name` char(32) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `description` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `company` char(16) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `dataInterface` char(32) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` char(32) NOT NULL,
+  `description` varchar(128) NOT NULL,
+  `company` char(16) NOT NULL,
+  `dataInterface` char(32) NOT NULL,
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `StationPlanningStatus`
+--
+
+CREATE TABLE `StationPlanningStatus` (
+  `name` char(32) NOT NULL,
+  `description` varchar(128) NOT NULL,
   PRIMARY KEY (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -52,22 +57,23 @@ CREATE TABLE `Nozzle` (
 
 CREATE TABLE `Station` (
   `id` mediumint(9) NOT NULL AUTO_INCREMENT,
-  `name` char(32) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `company` char(16) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `supplier` char(16) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `name` char(32) NOT NULL,
+  `company` char(16) NOT NULL,
+  `supplier` char(16) DEFAULT NULL,
   `location` point NOT NULL,
-  `address` char(128) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `address` char(128) NOT NULL,
   `num35MPa` tinyint(1) NOT NULL DEFAULT '1',
   `num70MPa` tinyint(1) NOT NULL DEFAULT '1',
   `numHiMPa` tinyint(1) NOT NULL DEFAULT '0',
   `nozzle` char(32) COLLATE utf8mb4_unicode_ci DEFAULT 'Generic',
   `dateOpened` date NOT NULL,
-  `planningStatus` tinyint(4) DEFAULT NULL,
+  `planningStatus` char(32) DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE KEY `name` (`name`),
   CONSTRAINT `fk_Station_company_Company` FOREIGN KEY (`company`) REFERENCES `Company` (`name`),
   CONSTRAINT `fk_Station_nozzle_Nozzle` FOREIGN KEY (`nozzle`) REFERENCES `Nozzle` (`name`),
-  CONSTRAINT `fk_Station_supplier_Company` FOREIGN KEY (`supplier`) REFERENCES `Company` (`name`);
+  CONSTRAINT `fk_Station_supplier_Company` FOREIGN KEY (`supplier`) REFERENCES `Company` (`name`),
+  CONSTRAINT `fk_Station_planningStatus_StationPlanningStatus` FOREIGN KEY (`planningStatus`) REFERENCES `StationPlanningStatus` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -85,6 +91,22 @@ CREATE TABLE `StationStatus` (
   `status35MPa` tinyint(1) NOT NULL,
   `status70MPa` tinyint(1) NOT NULL,
   `statusHiMPa` tinyint(1) NOT NULL DEFAULT '0',
-  CONSTRAINT `fk_StationStatus_stationID_Station` FOREIGN KEY (`stationID`) REFERENCES `Station` (`id`);
+  PRIMARY KEY (`stationID`, `recordedAt`),
+  CONSTRAINT `fk_StationStatus_stationID_Station` FOREIGN KEY (`stationID`) REFERENCES `Station` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `StationOpenPeriods`
+--
+
+CREATE TABLE `StationOpenPeriods` (
+  `stationID` mediumint(9) NOT NULL,
+  `day` enum('Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday') NOT NULL,
+  `start` char(4) NOT NULL DEFAULT '000',
+  `end` char(4) NOT NULL DEFAULT '2359',
+  `comments` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`stationID`, `day`),
+  CONSTRAINT `fk_StationOpenPeriods_stationID_Station` FOREIGN KEY (`stationID`) REFERENCES `Station`(`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
---
+++ settings-sample.php
@@ -0,0 +1,2 @@
+// Station data API URL
+$apiUrl = '';
