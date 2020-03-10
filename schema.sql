1cb173b2d24b704e652a347588f6fb8eef81c2e8
Billy Lin <nixon.software@gmail.com>
2020-03-10 03:07:15 -0700

    Clean up SQL Fix design document

--- schema.sql
+++ schema.sql
@@ -2,10 +2,6 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 
---
--- Database: `nixoumcj_energy_manager`
---
-
 -- --------------------------------------------------------
 
 --
